3d83837b2db4061a45e22ee7325aeae52d46f3f8
Greg Albers <galbers@getty.edu>
2022-02-16 09:18:32 -0800

    Add styles and scripts for accordion sections in the main chapters
    Plus two sample chapters with the required structure for the <section> elements

--- static/js/custom.js
+++ static/js/custom.js
@@ -28,77 +28,229 @@ function filterImageGrid() {
 }
 
 // From https://inclusive-components.design/collapsible-sections/
+// with some modifications:
+//
+// I added a check to only create the accordion heading button when there was
+// content following the heading. This is important in the vocabulary section
+// where not all translations have content following them.
+//
+// I also added:
+// .accordion-expander class on button
+// .display-accordion class on visible div
+//
+// And like with the prototype for this book, I'm repeating the code three
+// times to get H4, H3, and H2 elements (in that order) but there's surely a
+// more efficient way that would avoid all the duplication.
+
+(function() {
+  // Get all the <h4> headings
+  const headings = document.querySelectorAll('.quire-page--accordion .content h4')
+
+  Array.prototype.forEach.call(headings, heading => {
+
+    if ( heading.nextElementSibling && heading.nextElementSibling.tagName !== 'H4' ) {
+
+      // Give each <h4> a toggle button child
+      // with the SVG plus/minus icon
+      heading.innerHTML = `
+        <button class="accordion-expander" aria-expanded="false">
+          ${heading.innerHTML}
+          <svg aria-hidden="true" focusable="false" viewBox="0 0 10 10">
+            <rect class="vert" height="8" width="2" y="1" x="4"/>
+            <rect height="2" width="8" y="4" x="1"/>
+          </svg>
+        </button>
+      `
+
+      // Function to create a node list
+      // of the content between this <h4> and the next
+      const getContent = (elem) => {
+        let elems = []
+        while (elem.nextElementSibling && elem.nextElementSibling.tagName !== 'H4' && elem.nextElementSibling.tagName !== 'H3' && elem.nextElementSibling.tagName !== 'H2') {
+          elems.push(elem.nextElementSibling)
+          elem = elem.nextElementSibling
+        }
+
+        // Delete the old versions of the content nodes
+        elems.forEach((node) => {
+          node.parentNode.removeChild(node)
+        })
+
+        return elems
+      }
+
+      // Assign the contents to be expanded/collapsed (array)
+      let contents = getContent(heading)
+
+      // Create a wrapper element for `contents` and hide it
+      let wrapper = document.createElement('div')
+      // wrapper.style.display = 'none'
+      wrapper.hidden = true
+
+      // Add each element of `contents` to `wrapper`
+      contents.forEach(node => {
+        wrapper.appendChild(node)
+      })
+
+      // Add the wrapped content back into the DOM
+      // after the heading
+      heading.parentNode.insertBefore(wrapper, heading.nextElementSibling)
+
+      // Assign the button
+      let btn = heading.querySelector('button')
+
+      btn.onclick = () => {
+        // Cast the state as a boolean
+        let expanded = btn.getAttribute('aria-expanded') === 'true' || false
+
+        // Switch the state
+        btn.setAttribute('aria-expanded', !expanded)
+        // Switch the content's visibility
+        wrapper.hidden = expanded
+        wrapper.classList.toggle('display-accordion')
+      }
+    }
+  })
+})();
+
 
 (function() {
   // Get all the <h3> headings
-  const headings = document.querySelectorAll('.quire-page .content .accordion h3')
+  const headings = document.querySelectorAll('.quire-page--accordion .content h3, .quire-page .content .accordion h3')
 
   Array.prototype.forEach.call(headings, heading => {
-    // Give each <h3> a toggle button child
-    // with the SVG plus/minus icon
-    let headingText = heading.innerHTML
-    heading.innerHTML = `
-      <button aria-expanded="false">
-        <span class="heading-text">${headingText}</span>
-        <svg aria-hidden="true" focusable="false" viewBox="0 0 10 10">
-          <rect class="vert" height="8" width="2" y="1" x="4"/>
-          <rect height="2" width="8" y="4" x="1"/>
-        </svg>
-      </button>  `
 
-    // Function to create a node list
-    // of the content between this <h3> and the next
-    const getContent = (elem) => {
-      let elems = []
-      while (elem.nextElementSibling && elem.nextElementSibling.tagName !== 'H3' && elem.nextElementSibling.tagName !== 'H2') {
-        elems.push(elem.nextElementSibling)
-        elem = elem.nextElementSibling
+    if ( heading.nextElementSibling && heading.nextElementSibling.tagName !== 'H3' ) {
+
+      // Give each <h3> a toggle button child
+      // with the SVG plus/minus icon
+      heading.innerHTML = `
+        <button class="accordion-expander" aria-expanded="false">
+          ${heading.innerHTML}
+          <svg aria-hidden="true" focusable="false" viewBox="0 0 10 10">
+            <rect class="vert" height="8" width="2" y="1" x="4"/>
+            <rect height="2" width="8" y="4" x="1"/>
+          </svg>
+        </button>
+      `
+
+      // Function to create a node list
+      // of the content between this <h3> and the next
+      const getContent = (elem) => {
+        let elems = []
+        while (elem.nextElementSibling && elem.nextElementSibling.tagName !== 'H3' && elem.nextElementSibling.tagName !== 'H2') {
+          elems.push(elem.nextElementSibling)
+          elem = elem.nextElementSibling
+        }
+
+        // Delete the old versions of the content nodes
+        elems.forEach((node) => {
+          node.parentNode.removeChild(node)
+        })
+
+        return elems
       }
 
-      // Delete the old versions of the content nodes
-      elems.forEach((node) => {
-        node.parentNode.removeChild(node)
+      // Assign the contents to be expanded/collapsed (array)
+      let contents = getContent(heading)
+
+      // Create a wrapper element for `contents` and hide it
+      let wrapper = document.createElement('div')
+      wrapper.hidden = true
+
+      // Add each element of `contents` to `wrapper`
+      contents.forEach(node => {
+        wrapper.appendChild(node)
       })
 
-      return elems
+      // Add the wrapped content back into the DOM
+      // after the heading
+      heading.parentNode.insertBefore(wrapper, heading.nextElementSibling)
+
+      // Assign the button
+      let btn = heading.querySelector('button')
+
+      btn.onclick = () => {
+        // Cast the state as a boolean
+        let expanded = btn.getAttribute('aria-expanded') === 'true' || false
+
+        // Switch the state
+        btn.setAttribute('aria-expanded', !expanded)
+        // Switch the content's visibility
+        wrapper.hidden = expanded
+        wrapper.classList.toggle('display-accordion')
+      }
     }
+  })
+})();
 
-    // Assign the contents to be expanded/collapsed (array)
-    let contents = getContent(heading)
 
-    // If there are no contents add a class so that
-    // the svg can be hidden with CSS.
-    // TO DO: would obviousy be better not to create the button
-    // in the first place
-    if (contents.length == 0) {
-      heading.classList.add("no-content-expander");
+(function() {
+  // Get all the <h2> headings
+  const headings = document.querySelectorAll('.quire-page--accordion .content h2.fold-up')
+
+  Array.prototype.forEach.call(headings, heading => {
+
+    if ( heading.nextElementSibling && heading.nextElementSibling.tagName !== 'H2' ) {
+
+      // Give each <h2> a toggle button child
+      // with the SVG plus/minus icon
+      heading.innerHTML = `
+        <button class="accordion-expander" aria-expanded="false">
+          ${heading.innerHTML}
+          <svg aria-hidden="true" focusable="false" viewBox="0 0 10 10">
+            <rect class="vert" height="8" width="2" y="1" x="4"/>
+            <rect height="2" width="8" y="4" x="1"/>
+          </svg>
+        </button>
+      `
+
+      // Function to create a node list
+      // of the content between this <h2> and the next
+      const getContent = (elem) => {
+        let elems = []
+        while (elem.nextElementSibling && elem.nextElementSibling.tagName !== 'H3' && elem.nextElementSibling.tagName !== 'H2') {
+          elems.push(elem.nextElementSibling)
+          elem = elem.nextElementSibling
+        }
+
+        // Delete the old versions of the content nodes
+        elems.forEach((node) => {
+          node.parentNode.removeChild(node)
+        })
+
+        return elems
+      }
+
+      // Assign the contents to be expanded/collapsed (array)
+      let contents = getContent(heading)
+
+      // Create a wrapper element for `contents` and hide it
+      let wrapper = document.createElement('div')
+      wrapper.hidden = true
+
+      // Add each element of `contents` to `wrapper`
+      contents.forEach(node => {
+        wrapper.appendChild(node)
+      })
+
+      // Add the wrapped content back into the DOM
+      // after the heading
+      heading.parentNode.insertBefore(wrapper, heading.nextElementSibling)
+
+      // Assign the button
+      let btn = heading.querySelector('button')
+
+      btn.onclick = () => {
+        // Cast the state as a boolean
+        let expanded = btn.getAttribute('aria-expanded') === 'true' || false
+
+        // Switch the state
+        btn.setAttribute('aria-expanded', !expanded)
+        // Switch the content's visibility
+        wrapper.hidden = expanded
+        wrapper.classList.toggle('display-accordion')
+      }
     }
-
-    // Create a wrapper element for `contents` and hide it
-    let wrapper = document.createElement('div')
-    wrapper.hidden = true
-
-    // Add each element of `contents` to `wrapper`
-    contents.forEach(node => {
-      wrapper.appendChild(node)
-    })
-
-    // Add the wrapped content back into the DOM
-    // after the heading
-    heading.parentNode.insertBefore(wrapper, heading.nextElementSibling)
-
-    // Assign the button
-    let btn = heading.querySelector('button')
-
-    btn.onclick = () => {
-      // Cast the state as a boolean
-      let expanded = btn.getAttribute('aria-expanded') === 'true' || false
-
-      // Switch the state
-      btn.setAttribute('aria-expanded', !expanded)
-      // Switch the content's visibility
-      wrapper.hidden = expanded
-    }
-
   })
 })();
