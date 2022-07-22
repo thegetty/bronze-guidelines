c65d0c4f62e7cc6c3aabe18aa5ea4a216f31c6f1
Greg Albers <galbers@getty.edu>
2022-07-21 18:09:23 -0700

    Build custom def shortcode for popups

--- _plugins/shortcodes/def.js
+++ _plugins/shortcodes/def.js
@@ -1,14 +1,49 @@
-//
-// CUSTOMIZED FILE -- Bronze Guidelines
-//
+const { renderOneLine } = require('~lib/common-tags')
+
 /**
- * Temporary shortcode to replace q-def
+ * Shortcode to display pop-up vocab definitions with links to full vocab page
  *
- * @param      {String}  term  Matching a term in the vocabulary section
- *
- * @return     {boolean}  A styled HTML <span> element with the term
+ * @param      {String}   term      Matching a term in the vocabulary section
+ * @param      {String}   display   An optional override for inline display
+ * @return     {boolean}  An HTML <span> element copying the citation pop-up
  */
-module.exports = function (eleventyConfig, { page }) {
-  console.log("def --" + page.url)
-  return (term, display) => display ? `<span style="color: tomato;">${display} [${term}]</span>` : `<span style="color: tomato;">${term}</span>`
+module.exports = function (eleventyConfig, { collections, page }) {
+  const markdownify = eleventyConfig.getFilter('markdownify')
+
+  return function (term, display) {
+    for (const entry of collections.vocabulary ) {
+      if (entry.data.title == term) {
+        const displayText = display ? display : term
+
+        // strip def and cite shortcodes and show display value only
+        const regex = /\{% [a-z]+ (\"([^\"]*?)\" ){1,2}%\}/g
+        const plainDefinition = entry.data.definition.replace(regex, '$2')
+
+        // truncate definition but not mid-word
+        const maxLength = 240
+        let displayDefinition = ''
+        if (plainDefinition.length < maxLength){
+          displayDefinition = plainDefinition
+        } else {
+          displayDefinition = plainDefinition.substring(0, maxLength)
+          displayDefinition = displayDefinition
+            .substring(0, Math.min(
+              displayDefinition.length,
+              displayDefinition.lastIndexOf(' ')))
+            .concat(' ...')
+        }
+
+        return renderOneLine`
+          <span class="quire-citation quire-definition expandable">
+            <span class="quire-citation__button quire-definition__button" role="button" tabindex="0" aria-expanded="false">
+              ${displayText}
+            </span>
+            <span hidden class="quire-citation__content quire-definition__content">
+              <span class="visually-hidden">Definition: </span>
+              <strong>${entry.data.title}: </strong>${markdownify(displayDefinition)} <a href="${entry.url}" class="quire-definition__content__more-link">More</a>
+            </span>
+          </span>`
+      }
+    }
+  }
 }
