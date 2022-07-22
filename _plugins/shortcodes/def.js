625116dbea746d0ff19195439f32861aa6b80405
Greg Albers <galbers@getty.edu>
2022-07-22 08:59:04 -0700

    Rewrite function with find() instead of for and if

--- _plugins/shortcodes/def.js
+++ _plugins/shortcodes/def.js
@@ -13,46 +13,42 @@ module.exports = function (eleventyConfig, { collections, page }) {
   const markdownify = eleventyConfig.getFilter('markdownify')
 
   return function (term, display) {
-    let isTerm = false
-    for ( const entry of collections.vocabulary ) {
-      if (entry.data.title == term) {
-        isTerm = true
-        const displayText = display ? display : term
+    const displayText = display ? display : term
+    const vocabPage = collections.vocabulary
+      .find( entry => entry.data.title == term )
 
-        // strip def and cite shortcodes and show display value only
-        const regex = /\{% [a-z]+ (\"([^\"]*?)\" ){1,3}%\}/g
-        const plainDefinition = entry.data.definition.replace(regex, '$2')
-
-        // truncate definition but not mid-word
-        const maxLength = 240
-        let displayDefinition = ''
-        if (plainDefinition.length < maxLength){
-          displayDefinition = plainDefinition
-        } else {
-          displayDefinition = plainDefinition.substring(0, maxLength)
-          displayDefinition = displayDefinition
-            .substring(0, Math.min(
-              displayDefinition.length,
-              displayDefinition.lastIndexOf(' ')))
-            .concat(' ...')
-        }
-
-        return renderOneLine`
-          <span class="quire-citation quire-definition expandable">
-            <span class="quire-citation__button quire-definition__button" role="button" tabindex="0" aria-expanded="false">
-              ${displayText}
-            </span>
-            <span hidden class="quire-citation__content quire-definition__content">
-              <span class="visually-hidden">Definition: </span>
-              <strong>${entry.data.title}: </strong>${markdownify(displayDefinition)} <a href="${entry.url}" class="quire-definition__content__more-link">More</a>
-            </span>
-          </span>`
-      }
-    }
-    if ( isTerm == false ) {
+    if ( !vocabPage ) {
       warn(`Vocabulary page not found for '${term}' on ${page.inputPath}`)
-
-      return renderOneLine`<span style="color: tomato;">${term}</span>`
+      return renderOneLine`<span style="color:red;">${displayText}</span>`
     }
+
+    // strip def and cite shortcodes and show display value only
+    const regex = /\{% [a-z]+ (\"([^\"]*?)\" ){1,3}%\}/g
+    const plainDefinition = vocabPage.data.definition.replace(regex, '$2')
+
+    // truncate definition but not mid-word
+    const maxLength = 240
+    let displayDefinition = ''
+    if (plainDefinition.length < maxLength){
+      displayDefinition = plainDefinition
+    } else {
+      displayDefinition = plainDefinition.substring(0, maxLength)
+      displayDefinition = displayDefinition
+        .substring(0, Math.min(
+          displayDefinition.length,
+          displayDefinition.lastIndexOf(' ')))
+        .concat(' ...')
+    }
+
+    return renderOneLine`
+      <span class="quire-citation quire-definition expandable">
+        <span class="quire-citation__button quire-definition__button" role="button" tabindex="0" aria-expanded="false">
+          ${displayText}
+        </span>
+        <span hidden class="quire-citation__content quire-definition__content">
+          <span class="visually-hidden">Definition: </span>
+          <strong>${vocabPage.data.title}: </strong>${markdownify(displayDefinition)} <a href="${vocabPage.url}" class="quire-definition__content__more-link">More</a>
+        </span>
+      </span>`
   }
 }
