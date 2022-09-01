a7f10635475dca6d81b7ee5ccfdf125771763b78
Greg Albers <galbers@getty.edu>
2022-09-01 09:20:00 -0700

    Add sample table and table reference link to page

--- _plugins/shortcodes/figureRef.js
+++ _plugins/shortcodes/figureRef.js
@@ -34,16 +34,28 @@ module.exports = function(eleventyConfig) {
       warn(`NoId: Figure 'ref' shortcode must include one or more values corresponding to the 'id' of a figure on the page. @example {% ref 'fig-1', 'fig-7', 'fig-11' %}`)
     }
 
-    const label = idArray.length > 1 ? 'figs.' : 'fig.'
+    let label = ""
+    let icon = ""
+    if (ids.includes("fig")) {
+      label = idArray.length > 1 ? 'figs.' : 'fig.'
+    } else if (ids.includes("table")) {
+      label = idArray.length > 1 ? 'tables' : 'table'
+      icon = `<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20"><path d="M3 17V3h14v14Zm1.5-9.562h11V4.5h-11Zm4.167 4.041h2.666V8.938H8.667Zm0 4.021h2.666v-2.521H8.667ZM4.5 11.479h2.667V8.938H4.5Zm8.333 0H15.5V8.938h-2.667ZM4.5 15.5h2.667v-2.521H4.5Zm8.333 0H15.5v-2.521h-2.667Z"/></svg>`
+    } else if (ids.includes("video")) {
+      label = idArray.length > 1 ? 'videos' : 'video'
+      icon = `<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20"><path d="m8 13.5 5.5-3.5L8 6.5ZM3.5 16q-.625 0-1.062-.438Q2 15.125 2 14.5v-9q0-.625.438-1.062Q2.875 4 3.5 4h13q.625 0 1.062.438Q18 4.875 18 5.5v9q0 .625-.438 1.062Q17.125 16 16.5 16Zm0-1.5h13v-9h-13v9Zm0 0v-9 9Z"/></svg>`
+    }
 
     // transform the array of figure ids into and array of markdown links
     const links = idArray.map((id, index) => {
       id = id.trim()
-      let text = id.replace(/^fig-[0]{0,2}/i, '')
+      let text = id.replace(/^[a-z]+-[0]{0,2}/i, '')
       if (index === 0) text = `${label} ${text}`
       return `[${text}](#${id}){.q-figure__modal-link}`
     })
 
-    return oneLineCommaLists`${links}`
+    const allLinks = oneLineCommaLists`${links}`
+
+    return `${allLinks}${icon}`
   }
 }
