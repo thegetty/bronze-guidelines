64444f1ebcca18555d86ae3f9e0d96096f8c4d71
Greg Albers <galbers@getty.edu>
2024-11-14 14:57:04 -0800

    Make final round of edits for PDF

--- _includes/components/figure/caption.js
+++ _includes/components/figure/caption.js
@@ -1,6 +1,7 @@
 //
 // CUSTOMIZED FILE -- Bronze Guidelines
 // removed hard-coded <em> tags in embed url, line 22
+// add embed link to select images with embed_true
 //
 const { oneLine } = require('~lib/common-tags')
 
@@ -16,11 +17,21 @@ const { oneLine } = require('~lib/common-tags')
 module.exports = function(eleventyConfig) {
   const markdownify = eleventyConfig.getFilter('markdownify')
   const figureMediaEmbedUrl = eleventyConfig.getFilter('figureMediaEmbedUrl')
-  return function({ caption, credit, content='', mediaId, mediaType}) {
-    const { sourceUrl } = figureMediaEmbedUrl({ mediaId, mediaType })
-    const mediaSourceLink = sourceUrl
+  return function({ caption, credit, content='', embedLink, id, mediaId, mediaType}) {
+    let mediaSourceLink = ''
+    if (embedLink) {
+      const sourceUrl = `https://www.getty.edu/publications/bronze-guidelines/visual-atlas/${id.replace('fig-', '')}/` 
+      mediaSourceLink = sourceUrl
       ? `<span class="q-figure__caption-embed-link"><a href="${sourceUrl}">${sourceUrl}</a></span>`
       : ''
+    } else {
+      const { sourceUrl } = figureMediaEmbedUrl({ mediaId, mediaType })
+      mediaSourceLink = sourceUrl
+      ? `<span class="q-figure__caption-embed-link"><a href="${sourceUrl}">${sourceUrl}</a></span>`
+      : ''
+    }
+    console.log("mediaSourceLink :: " + mediaSourceLink)
+
     return oneLine`
       <figcaption class="q-figure__caption">
         ${mediaSourceLink}
